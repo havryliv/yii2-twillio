e65b18f6d549e8be4ea01d6af6a1090d25029dea
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
2015-10-21 14:46:39 +0200

    Add AccessToken service

--- Twillio.php
+++ Twillio.php
@@ -13,8 +13,12 @@ class Twillio extends Component
     public $sid;
     public $token;
 
+    public $signingKeySid;
+    public $signingKeySecret;
+
     private $_client = null;
     private $_clientCapability = null;
+    private $_accessToken = null;
 
     public function init()
     {
@@ -26,25 +30,42 @@ class Twillio extends Component
         }
     }
 
+    /**
+     * @return null|\Services_Twilio
+     */
     public function getClient()
     {
         if ($this->_client === null) {
             $client = new \Services_Twilio($this->sid, $this->token);
-
             $this->_client = $client;
         }
 
         return $this->_client;
     }
 
+    /**
+     * @return null|\Services_Twilio_Capability
+     */
     public function getClientCapability()
     {
         if ($this->_clientCapability === null) {
             $client = new \Services_Twilio_Capability($this->sid, $this->token);
-
             $this->_clientCapability = $client;
         }
 
         return $this->_clientCapability;
+    }
+
+    /**
+     * @return null|\Services_Twilio_AccessToken
+     */
+    public function getAccessToken() {
+        if ($this->_accessToken === null) {
+            $client = new \Services_Twilio_AccessToken($this->signingKeySid, $this->sid, $this->signingKeySecret);
+            $this->_accessToken = $client;
+        }
+
+        return $this->_accessToken;
+        
     }
 } 
